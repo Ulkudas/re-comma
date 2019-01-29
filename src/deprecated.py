a5c42fd6ed53687ec5bce6a29877afa575db794a
Enes Keles <eneskeles81@gmail.com>
2019-01-29 23:41:03 +0300

    Add files via upload

---
+++ src/deprecated.py
@@ -0,0 +1,104 @@
+"""
+TODO burda dursun belki ise yara
+def get_data(path):
+    files = os.listdir(path)
+    movie_tokens = dict()
+    for filename in files:
+        movie_name = filename[7:-4]
+        with open(os.path.join(path,filename), 'r') as myfile:
+            data = myfile.read().replace('\n', '')
+
+            data = filter_annotations(data)
+            data = filter_stopwords(data)
+            data = stem_script(data)
+            movie_tokens[movie_name] = data
+            print(len(data))
+    return movie_tokens
+
+def filter_annotations(scriptString):
+    s = scriptString.replace('VOICE', '')
+    s = s.replace('(O.S.)', '')
+    s = s.replace('NORM', '')
+    s = s.replace('(V.O.)', '')
+    s = s.replace('CUT TO:', '')
+    return s
+
+def filter_stopwords(script_string):
+    extra_stopwords = ['\'ve', '...', '--', '\'s', '\'m', '\'d', 'n\'t', '\'\'']
+    stop_words = set(stopwords.words('english'))
+
+    for stopword in extra_stopwords:
+        stop_words.add(stopword)
+    script_tokens = word_tokenize(script_string.lower())
+    filtered_script = []
+
+    for w in script_tokens:
+        if w not in stop_words and w not in string.punctuation:
+            filtered_script.append(w)
+
+    return filtered_script
+
+def stem_script(script_tokens):
+    stemmed_script = []
+    for w in script_tokens:
+        stemmed_script.append(stem(w))
+    return stemmed_script
+
+"""
+
+
+
+'''
+import math
+import re
+import string
+
+from textblob import TextBlob as tb
+import os
+
+import preprocess
+
+
+def tf(word, blob):
+    return blob.words.count(word) / len(blob.words)
+
+def n_containing(word, bloblist):
+    return sum(1 for blob in bloblist if word in blob.words)
+
+def idf(word, bloblist):
+    return math.log(len(bloblist) / (1 + n_containing(word, bloblist)))
+
+def tfidf(word, blob, bloblist):
+    return tf(word, blob) * idf(word, bloblist)
+
+def get_script(base_path, filename):
+    with open(os.path.join(base_path, filename), 'r') as myfile:
+        data = myfile.read().lower()
+        data = re.sub('[' + string.punctuation + ']+', '', data)
+        data = re.sub('[\s]+', ' ', data)
+        data = data.strip().split(' ')
+        movie_script = tb(' '.join(data))
+
+    return movie_script
+
+
+def get_script_lists(base_path):
+    filenames = os.listdir(base_path)
+    script_lists = []
+    for count, filename in enumerate(filenames):
+        script_lists.append(get_script(base_path, filename))
+    return script_lists
+
+
+current_path = os.path.dirname(os.path.abspath(__file__))
+base_path = os.path.join(current_path, 'resources3')
+movie_names = preprocess.get_movie_names(base_path, [])
+scripts = get_script_lists(base_path)
+
+for i, blob in enumerate(scripts):
+    print("Top words in", movie_names[i])
+    scores = {word: tfidf(word, blob, scripts) for word in blob.words}
+    sorted_words = sorted(scores.items(), key=lambda x: x[1], reverse=True)
+    for word, score in sorted_words[:3]:
+        print("\tWord: {}, TF-IDF: {}".format(word, round(score, 5)))
+        '''
